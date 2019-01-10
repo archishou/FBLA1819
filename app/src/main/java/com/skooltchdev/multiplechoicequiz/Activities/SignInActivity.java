4d3d7f955eb47ee6ea5e9a96f29fc3dcc0c08b85
Archishmaan Peyyety <archishou@gmail.com>
2019-01-10 11:26:46 -0500

    buttns for sign in

--- app/src/main/java/com/skooltchdev/multiplechoicequiz/Activities/SignInActivity.java
+++ app/src/main/java/com/skooltchdev/multiplechoicequiz/Activities/SignInActivity.java
@@ -1,7 +1,10 @@
 package com.skooltchdev.multiplechoicequiz.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.skooltchdev.multiplechoicequiz.R;
 
@@ -10,9 +13,27 @@ import com.skooltchdev.multiplechoicequiz.R;
  * Project: FBLA1819
  */
 public class SignInActivity extends AppCompatActivity {
+    private Button signIn, forgotPassword, signUp;
     @Override
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_signin);
+        signIn = (Button) findViewById(R.id.signInButton);
+        forgotPassword = (Button) findViewById(R.id.forgotPasswordButton);
+        signUp = (Button) findViewById(R.id.noAccountButton);
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
