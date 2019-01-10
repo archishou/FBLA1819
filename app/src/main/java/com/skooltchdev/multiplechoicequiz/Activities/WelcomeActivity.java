784a3bde47ffeca901823540c4fa12838ee50689
Archishmaan Peyyety <archishou@gmail.com>
2019-01-10 01:38:50 -0500

    updated gradle

---
+++ app/src/main/java/com/skooltchdev/multiplechoicequiz/Activities/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package com.skooltchdev.multiplechoicequiz.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.skooltchdev.multiplechoicequiz.R;
+
+/**
+ * Created by Archishmaan Peyyety on 1/10/19.
+ * Project: FBLA1819
+ */
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_welcome);
+    }
+}
