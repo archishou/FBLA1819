d2f7027da9f9fee2eeb4e1db45c10b8716d50789
Archishmaan Peyyety <archishou@gmail.com>
2019-01-10 11:08:25 -0500

    more activitys

---
+++ app/src/main/java/com/skooltchdev/multiplechoicequiz/Activities/RegisterActivity.java
@@ -0,0 +1,10 @@
+package com.skooltchdev.multiplechoicequiz.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Archishmaan Peyyety on 1/10/19.
+ * Project: FBLA1819
+ */
+public class RegisterActivity extends AppCompatActivity {
+}
