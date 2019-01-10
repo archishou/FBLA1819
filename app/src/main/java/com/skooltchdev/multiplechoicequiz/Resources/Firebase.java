357e7cce5012954ce10c0f934688627e23a51f95
Archishmaan Peyyety <archishou@gmail.com>
2019-01-10 14:45:22 -0500

    reason

--- app/src/main/java/com/skooltchdev/multiplechoicequiz/Resources/Firebase.java
+++ app/src/main/java/com/skooltchdev/multiplechoicequiz/Resources/Firebase.java
@@ -9,8 +9,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by Archishmaan Peyyety on 1/10/19.
@@ -21,6 +25,7 @@ public class Firebase {
     static FirebaseAuth firebaseAuth;
     static DatabaseReference databaseReference;
     static boolean isAuth;
+    static int score;
     static boolean mSuccessful;
     public static boolean isAuth(String userName, String password, Activity activity)  {
         FirebaseApp.initializeApp(activity.getApplicationContext());
@@ -62,10 +67,40 @@ public class Firebase {
         return ismSuccessful();
     }
 
+    public static FirebaseUser getUser () {
+        return FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    public int getColumnData(final String column) {
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(getUser().getUid()).child(column).getValue(Integer.class) != null)
+                setScore(dataSnapshot.child(getUser().getUid()).child(column).getValue(Integer.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        return getScore();
+    }
+
+    private static int getScore() {
+        return score;
+    }
+
+    private static void setScore(int score) {
+        Firebase.score = score;
+    }
+
     private static boolean ismSuccessful() {
         return mSuccessful;
     }
-
     private static void setmSuccessful(boolean mSuccessful) {
         Firebase.mSuccessful = mSuccessful;
     }
